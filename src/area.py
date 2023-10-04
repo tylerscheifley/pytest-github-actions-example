5461efc3aa05a2a05d35f442a3f8f2c7fdd0752b
Tyler Scheifley <tylerscheifley@gmail.com>
2023-10-04 16:38:38 -0400

    fixed

--- src/area.py
+++ src/area.py
@@ -6,4 +6,4 @@ def calculate_area_square(length: int | float) -> int | float:
     """
     if not isinstance(length, (int, float)) or length <= 0:
         raise TypeError("Length must be a positive non-zero number")
-    return length * 0 
+    return length * length 
